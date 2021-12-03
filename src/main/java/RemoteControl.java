05db5d17fa078334ea00c415feed91761d12bc35
Beciuch <bernadettapawlaczyk@dmail.com>
2021-12-03 13:06:05 +0100

    [add]  some new method

---
+++ src/main/java/RemoteControl.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class RemoteControl {
+
+    private final int HEIGHT = 16;
+    private final int WIDTH = 5;
+    private final int THICKNESS = 4;
+
+    private static final List<String> BUTTONS = List.of("1", "2", "3", "4", "5", "6", "7",
+            "8", "9", "Power", "VolumeUp", "VolumeDomn",
+            "NextChannel", "PreviousChannel");
+
+    private int currentVolume;
+    private int currentChannel;
+
+    public void volumeUp(){
+        currentVolume++;
+    }
+
+    public void volumeDown(){
+        currentVolume--;
+    }
+
+    public void nextChannel(){
+        currentChannel++;
+    }
+
+    public void  prevousChannel(){
+        currentChannel--;
+    }
+
+
+}
